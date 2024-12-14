3ed4ebb0482f3f6f364e4832d851f20269b707ef
Vincent CLAVIEN <v@s427.ch>
2024-12-14 16:27:12 +0100

    moved some JS code from HTML to main.js

--- js/main.js
+++ js/main.js
@@ -1477,6 +1477,37 @@ const drag = {
 
 // initialization
 
+const isFileProtocol = window.location.protocol === "file:";
+const scripts = [
+  {
+    src: "js/jszip.min.js",
+    integrity:
+      "sha512-XMVd28F1oH/O71fzwBnV7HucLxVwtxf26XV8P4wPk26EDxuGZ91N8bsOttmnomcCD3CS5ZMRL50H0GgOHvegtg==",
+    crossorigin: "anonymous",
+    defer: false,
+  },
+  {
+    src: "js/alpinejs.min.js",
+    integrity:
+      "sha512-FUaEyIgi9bspXaH6hUadCwBLxKwdH7CW24riiOqA5p8hTNR/RCLv9UpAILKwqs2AN5WtKB52CqbiePBei3qjKg==",
+    crossorigin: "anonymous",
+    defer: true,
+  },
+];
+
+scripts.forEach(({ src, integrity, crossorigin, defer }) => {
+  const script = document.createElement("script");
+  script.src = src;
+  if (!isFileProtocol) {
+    script.integrity = integrity;
+    script.crossOrigin = crossorigin;
+  }
+  if (defer) {
+    script.defer = true;
+  }
+  document.head.appendChild(script);
+});
+
 document.addEventListener("alpine:init", () => {
   Alpine.store("files", filesStore);
   Alpine.store("lightbox", lightboxStore);
