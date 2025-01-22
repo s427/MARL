84b13b77fc9063aaa8015437d6a04fe6e8736a88
Vincent CLAVIEN <v@s427.ch>
2025-01-22 19:52:28 +0100

    earlier loading of language files

--- dev/public/js/i18n/_langs.js
+++ dev/public/js/i18n/_langs.js
@@ -2,3 +2,9 @@ const appLangs = {
   en: "English",
   fr: "Français",
 };
+
+for (const lang in appLangs) {
+  const script = document.createElement("script");
+  script.src = `js/i18n/${lang}.js`;
+  document.head.appendChild(script);
+}
